b3a128d30a3c0e26803719bb5dfefe2be3ff1b77
dev-sonen <dev.bensondimasupil@gmail.com>
2025-01-06 09:39:16 +0800

    initial commit

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 
+    import { app , pages } from '@microsoft/teams-js'
+
     useHead( {
         title: 'nuxt app',
         meta: [
@@ -8,6 +10,15 @@
                 content: 'nuxtjs application for teams'
             }
         ]
+    } )
+
+    onMounted( () => {
+
+        app.initialize().then( () => {
+            pages.config.setValidityState( true )
+            app.notifySuccess()
+        } )
+
     } )
     
 </script>
